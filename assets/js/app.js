be1c2926e862a4758516b1565acc54aa7c40567c
uldis <uldis@kood.pro>
2025-04-16 15:41:48 +0300

    Order of vbiew

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,6 +21,7 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
+// Hooks
 import ScrollToBottom from "./hooks/scroll_to_bottom"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
